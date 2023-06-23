f9036ad109bc1f08b0d417b7574ee8ec89a7e592
RadekKusiak71 <radoslawkusiak12@gmail.com>
2023-06-23 02:45:00 +0200

    Authentications urls added

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.HomePage.as_view(),name='home_page')
+    path('',views.HomePage.as_view(),name='home_page'),
+
+    # Authentication urls
+    path('register/',views.RegisterPage.as_view(),name='register_page'),
+    path('login/',views.LoginPage.as_view(),name='login_page'),
+    path('logout/',views.LogoutRequest.as_view(),name='logout_request'),
 ]
