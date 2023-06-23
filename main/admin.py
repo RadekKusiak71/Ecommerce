ae87e88f956b0e8722b205511bd03eefc5b72541
RadekKusiak71 <radoslawkusiak12@gmail.com>
2023-06-23 02:44:14 +0200

    DB tables registered

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Profile, Category, Product, Cart , CartItem, Order, OrderItem, Order_details
 
-# Register your models here.
+admin.site.register(Profile)
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Cart)
+admin.site.register(CartItem)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Order_details)
