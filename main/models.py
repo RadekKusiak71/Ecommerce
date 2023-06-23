298e592dbf86a408043e6b1dcd1e25e57ea9f4e4
RadekKusiak71 <radoslawkusiak12@gmail.com>
2023-06-23 02:43:51 +0200

    Initial Migrations

--- main/models.py
+++ main/models.py
@@ -1,3 +1,94 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import date
+from django.utils import timezone
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=9)
+
+    def __str__(self):
+        return f'{self.firstname} {self.lastname}'
+    
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    product_number = models.CharField(max_length=7)
+    price = models.IntegerField()
+    quantity = models.IntegerField()
+    is_available = models.BooleanField(default=True)
+
+    def __str__(self):
+        if self.is_available:
+            return f'{self.name} is available'
+        else:
+            return f'{self.name} is unavailable'
+    
+    def availability(self):
+        if self.quantity != 0:
+            self.is_available = True
+        else:
+            self.is_available = False
+        self.save()
+
+class Cart(models.Model):
+    owner = models.ForeignKey(Profile,on_delete=models.CASCADE,null=True)
+    session_key = models.CharField(max_length=255,null=True)
+
+    def __str__(self):
+        if self.owner:
+            return f'Cart for {self.owner.firstname} {self.owner.lastname}'
+        else:
+            return f'Cart for {self.session_key}'
+        
+class CartItem(models.Model):
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Item {self.product.name} for cart {self.cart.id}'
+    
+class Order(models.Model):
+    owner = models.ForeignKey(Profile,on_delete=models.CASCADE,null=True)
+    session_key = models.CharField(max_length=255,null=True)
+
+    def __str__(self):
+        if self.owner:
+            return f'Cart for {self.owner.firstname} {self.owner.lastname}'
+        else:
+            return f'Cart for {self.session_key}'
+        
+class OrderItem(models.Model):
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    order = models.ForeignKey('Order', on_delete=models.CASCADE)
+
+    def __str__(self):
+        if self.order.owner:
+            return f"{self.order.owner} - {self.product.name}"
+        else:
+            return f"{self.order.session_key} - {self.product.name}"
+        
+class Order_details(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone = models.CharField(max_length=9)
+    address = models.CharField(max_length=50)
+    created_at = models.DateTimeField(default=timezone.now)
+    status = models.BooleanField(default=False)
+    order = models.ForeignKey('Order',on_delete=models.CASCADE,null=True)
+
+    def __str__(self):
+        if self.status:
+            return f'Order for {self.first_name} {self.last_name} - status: Not Ready to send'
+        else:
+            return f'Order for {self.first_name} {self.last_name} - status: Ready to send'
