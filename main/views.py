c05b37731715ff518473c75ba73aa5f851b3760a
RadekKusiak71 <radoslawkusiak12@gmail.com>
2023-06-23 02:45:22 +0200

    Register/Login/Logout class added

--- main/views.py
+++ main/views.py
@@ -1,7 +1,70 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
 from django.views import View
+from django.contrib.auth import authenticate, login,logout
+from django.contrib import messages
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import RegisterForm
+
+from .models import Profile, Category, Product, Cart , CartItem, Order, OrderItem, Order_details
 
 class HomePage(View):
     def get(self,request):
         context = {}
         return render(request,'main/home_page.html',context)
+
+class RegisterPage(View):
+    def get(self,request):
+        form = RegisterForm()
+        context = {'register_form':form}
+        return render(request,'main/register_page.html',context)
+
+    def post(self,request):
+        form = RegisterForm(request.POST)
+        return self.form_validation(form,'login_page')
+    
+    def form_validation(self,form,succes_url):
+        if form.is_valid():
+            user = form.save()
+            Profile.objects.create(
+                user = user,
+                firstname = user.first_name,
+                lastname = user.last_name,
+                email = user.email,
+                phone_number = self.request.POST['phone_number']
+            )
+            return redirect(succes_url)
+        else:
+            return HttpResponse('Invalid data try again later')
+
+class LoginPage(View):
+    def get(self,request):
+        form = AuthenticationForm()
+        context = {'login_form':form}
+        return render(request,'main/login_page.html',context)
+    
+    def post(self,request):
+        form = AuthenticationForm(request,data=request.POST)
+        return  self.form_validation(form,'home_page')
+    
+    def form_validation(self,form,succes_url):
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username,password=password)
+            if user is not None:
+                login(self.request,user)
+                return redirect(succes_url)
+            else:
+                return HttpResponse('Invalid data try again later')
+        else:
+            return HttpResponse('Invalid data try again later')
+        
+
+class LogoutRequest(LoginRequiredMixin,View):
+    def get(self,request):
+        logout(request)
+        return redirect('home_page')
