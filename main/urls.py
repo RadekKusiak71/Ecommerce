83227f8140ead7c3786c2e74a8470f74037aa72c
RadekKusiak71 <radoslawkusiak12@gmail.com>
2023-06-25 01:44:18 +0200

    staic added media

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('',views.HomePage.as_view(),name='home_page'),
-
     # Authentication urls
     path('register/',views.RegisterPage.as_view(),name='register_page'),
     path('login/',views.LoginPage.as_view(),name='login_page'),
     path('logout/',views.LogoutRequest.as_view(),name='logout_request'),
-]
+    ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
